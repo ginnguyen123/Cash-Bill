d01d55193118ab4b39019c6f376a226e361b851c
ginnguyen123 <nhuunghia95@gmail.com>
2022-10-06 05:20:22 -0700

    fix1

--- cashBill.js
+++ cashBill.js
@@ -11,7 +11,6 @@ let products = [];
 const cashBill_data = "productdData";
 function init() {
     if (window.localStorage.getItem(cashBill_data) == null) {
-        let item = new Product(1,"Black Caffe",0,16000)
         products.push(item)
         window.localStorage.setItem(cashBill_data, JSON.stringify(products));
     }
@@ -29,10 +28,10 @@ function renderBill(){
                 <td class="text-center">${can.productPrice}</td>
                 <td class="text-center">${can.Amount}</td>
                 <td class="btnover">
-                    <button class="btn btnRemove" onclick="removeProduct()">Remove</button>
+                    <button class="btn btnRemove" onclick="removeProduct(${can.productId})">Remove</button>
                     <button class="btn btnEdit" onclick="editProduct(${can.productId})">Edit</button>
-                    <button class="btn" onclick="updateProduct(${can.productId})">Update</button>
-                    <button class="btn" onclick="cancelProduct(${can.productId})">Cancel</button>
+                    <button class="btn btnUpdate" onclick="updateProduct(${can.productId})">Update</button>
+                    <button class="btn btnCancel" onclick="cancelProduct(${can.productId})">Cancel</button>
                 </td>
             <tr>
         `
@@ -53,7 +52,7 @@ function addProduct(){
     products.push(newProduct);
     window.localStorage.setItem(cashBill_data, JSON.stringify(products));
     renderBill();
-
+    resetForm();
 }
 function priceProducts(){
     let prices = 0;
@@ -97,7 +96,7 @@ function totalAmount(){
     return totalAmount;
 }
 function removeProduct(productId){
-    let comfirmed = window.confirm("Bạn muốn xóa sản phẩm này?") // trả về bolean
+    let comfirmed = window.confirm("Bạn muốn xóa sản phẩm này?");
     console.log(comfirmed);
     if (comfirmed) {
         let index = products.findIndex(function (can) {
@@ -118,17 +117,17 @@ function editProduct(pdtId){
                                     <option value="Lemon Juice">Lemon Juice</option>
                                 </select>`
     tr.children[2].innerHTML = `<input type="number" id="quantity-${pdtId}" class="form-control">`
-    let action = document.getElementById(`action_${pdtId}`);
-    action.children[1].classList.removeProduct('d-none');
-    action.children[2].classList.removeProduct('d-none');
-    priceProducts();
-    renderBill();
+   // let action = document.getElementById(`tr_${pdtId}`);
+    //action.children[1].classList.removeProduct('d-none');
+    //action.children[2].classList.removeProduct('d-none');
+    //window.localStorage.setItem(cashBill_data, JSON.stringify(products));
+   
+    //renderBill();
 }
 function updateProduct(pdtId){
     console.log(products[pdtId-1])
     let updateProduct = document.getElementById(`product-${pdtId}`).value;
     let quantityProduct = Number(document.getElementById(`quantity-${pdtId}`).value);
-    console.log(quantityProduct)
     if(updateProduct != "" && isNaN(quantityProduct) == false){
         products[pdtId-1].productName = document.getElementById(`product-${pdtId}`).value;
         products[pdtId-1].productQuantity = document.getElementById(`quantity-${pdtId}`).value;
@@ -136,10 +135,15 @@ function updateProduct(pdtId){
         return alert("Nhập dữ liệu cần thay đổi!");
     }
     renderBill()
-    priceProducts();
+    window.localStorage.setItem(cashBill_data, JSON.stringify(products));
+    priceProducts(products[pdtId-1].productName);
 }
 function cancelProduct(){
     renderBill();
+}
+function resetForm(){
+    document.getElementById("product").value = "";
+    document.getElementById("quantity").value = "";
 }
 function ready(){
     init();
